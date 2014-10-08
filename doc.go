456eb531897d4bea0c42f464dd6923a89eaac6ad
Andrew Dunham <andrew@du.nham.ca>
2014-10-08 02:51:42 -0700

    A bit more documentation in the package

---
+++ doc.go
@@ -0,0 +1,4 @@
+// goscrape is a simple, extensible scraping library for Go.  For more
+// information, please read the README and examples on GitHub, and the
+// documentation for the ScrapeConfig and Scraper types.
+package scrape
