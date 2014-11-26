40df96344900ea7172f21ae31d10b31934537251
Andrew Dunham <andrew@du.nham.ca>
2014-11-25 17:39:27 -0800

    Add example scraping Hacker News, showing pagination

---
+++ _examples/scrape_hn.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/andrew-d/goscrape"
+)
+
+func main() {
+	numPages := 0
+
+	config := &scrape.ScrapeConfig{
+		DividePage: scrape.DividePageBySelector("tr:nth-child(3) tr:nth-child(3n-2):not([style='height:10px'])"),
+
+		Pieces: []scrape.Piece{
+			{Name: "title", Selector: "td.title > a", Extractor: scrape.TextExtractor{}},
+			{Name: "link", Selector: "td.title > a", Extractor: scrape.AttrExtractor{Attr: "href"}},
+			{Name: "rank", Selector: "td.title[align='right']",
+				Extractor: scrape.RegexExtractor{Regex: regexp.MustCompile(`(\d+)`)}},
+		},
+
+		// Extract the first 3 pages of results
+		NextPage: func(doc *goquery.Selection) string {
+			val, found := doc.Find("a[rel='nofollow']:last-child").Attr("href")
+
+			numPages++
+			if !found || numPages >= 3 {
+				return ""
+			}
+
+			return "https://news.ycombinator.com/" + val
+		},
+	}
+
+	scraper, err := scrape.New(config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating scraper: %s\n", err)
+		os.Exit(1)
+	}
+
+	results, err := scraper.Scrape("https://news.ycombinator.com")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error scraping: %s\n", err)
+		os.Exit(1)
+	}
+
+	json.NewEncoder(os.Stdout).Encode(results)
+}
